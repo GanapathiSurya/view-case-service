8a46585764fe22a43b69743f580ed218d5f6592f
DF-VIZAG1 <venkata.g@datafoundry.ai>
2022-09-12 22:12:43 +0530

    package 4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export { ViewCase } from "./components";
+import React from 'react';
+import { Viewcase } from './components/Viewcase';
+
+export default function ViewCase(props) {
+  return <Viewcase caseId={props.caseId} caseStatus={props.caseStatus}/>
+}
